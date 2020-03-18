2e21ef424825d83cda7dbd923def4391f974d1b1
Anthony <isonaj@gmail.com>
2020-03-19 09:03:20 +1000

    Axios call to load events from json-server

--- real-world-vue/src/views/EventList.vue
+++ real-world-vue/src/views/EventList.vue
@@ -1,15 +1,36 @@
 <template>
   <div>
     <h1>Event Listing</h1>
-    <EventCard></EventCard>
+    <EventCard
+      v-for="event in events"
+      :key="event.id"
+      :event="event"
+    ></EventCard>
   </div>
 </template>
 
 <script>
 import EventCard from '@/components/EventCard.vue';
+import axios from 'axios';
+
 export default {
   components: {
     EventCard
+  },
+  data() {
+    return {
+      events: []
+    };
+  },
+  created() {
+    axios
+      .get('http://localhost:3000/events')
+      .then(response => {
+        this.events = response.data;
+      })
+      .catch(error => {
+        console.log('There was an errpr: ' + error.response);
+      });
   }
 };
 </script>
