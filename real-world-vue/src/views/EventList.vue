3d588874d0de9f5cbba484d1fa5b33ba1c66ee9d
Anthony <isonaj@gmail.com>
2020-03-19 09:27:50 +1000

    Refactor axios calls to a service

--- real-world-vue/src/views/EventList.vue
+++ real-world-vue/src/views/EventList.vue
@@ -11,7 +11,7 @@
 
 <script>
 import EventCard from '@/components/EventCard.vue';
-import axios from 'axios';
+import EventService from '@/services/EventService.js';
 
 export default {
   components: {
@@ -23,8 +23,7 @@ export default {
     };
   },
   created() {
-    axios
-      .get('http://localhost:3000/events')
+    EventService.getEvents()
       .then(response => {
         this.events = response.data;
       })
