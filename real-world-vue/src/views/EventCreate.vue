6c5c798adae392238abb5d5fbd0c2944ff2a8b87
Anthony <isonaj@gmail.com>
2020-03-19 13:00:31 +1000

    Initial basic store access

--- real-world-vue/src/views/EventCreate.vue
+++ real-world-vue/src/views/EventCreate.vue
@@ -1,9 +1,23 @@
 <template>
-  <h1>Event Create</h1>
+  <div>
+    <h1>Create an Event, {{ userName }}</h1>
+    <p>This event was created by {{ userName }}</p>
+    <ul>
+      <li v-for="cat in categories" :key="cat">{{ cat }}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
-export default {};
+import { mapState } from 'vuex';
+
+export default {
+  computed: mapState({
+    userName: state => state.user.name,
+    userId: state => state.user.id,
+    categories: state => state.categories
+  })
+};
 </script>
 
 <style></style>
