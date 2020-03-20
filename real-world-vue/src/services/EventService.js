1e0a1db88ee31e4f2f3cf65205d8d3f3f9e9c0b6
Anthony <isonaj@gmail.com>
2020-03-20 20:54:23 +1000

    Update nprogress bar on event api calls

--- real-world-vue/src/services/EventService.js
+++ real-world-vue/src/services/EventService.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import NProgress from 'nprogress';
 
 const apiClient = axios.create({
   baseURL: 'http://localhost:3000',
@@ -9,6 +10,16 @@ const apiClient = axios.create({
   }
 });
 
+apiClient.interceptors.request.use(config => {
+  console.log('request');
+  NProgress.start();
+  return config;
+});
+apiClient.interceptors.response.use(response => {
+  console.log('response');
+  NProgress.done();
+  return response;
+});
 export default {
   getEvents(perPage, page) {
     return apiClient.get(`/events?_limit=${perPage}&_page=${page}`);
